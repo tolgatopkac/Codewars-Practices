0fc3f650c164ae752ac1d5b06f618f8912a25878
Tolga Topkaç <tolgatopkac09@gmail.com>
2021-08-24 06:13:04 +0300

    day#1-24.08.21

---
+++ 8kyu/21-Grasshopper - Function syntax debugging/app.js
@@ -0,0 +1,19 @@
+/* Grasshopper - Function syntax debugging
+A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them. */
+
+
+// Question 
+function main [verb, noun]
+  return verb + noun
+}
+
+// Solution 
+
+function main (verb, noun){
+  return verb + noun
+}
+
+
+// Alternative
+
+const main2 = (verb,noun) => verb+ noun
