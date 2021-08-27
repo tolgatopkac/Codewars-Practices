a653af6eeab952ed6a7e7ae4e293309e0372c923
Tolga Topkaç <tolgatopkac09@gmail.com>
2021-08-27 07:44:04 +0300

    day#3 26.08.21

---
+++ 8kyu/33 - Remove String Space/app.js
@@ -0,0 +1,8 @@
+/* Simple, remove the spaces from the string, then return the resultant string.*/
+function noSpace(x) {
+  return x.replace(/\s/g, "");
+}
+
+const noSpace = (x) => x.replace(/\s/g, "");
+
+const noSpace = (x) => x.split(" ").join("");
