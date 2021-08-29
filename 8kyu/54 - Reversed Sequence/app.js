047ef60e36c289064477cf6a50470aaaccf4b360
Tolga Topkaç <tolgatopkac09@gmail.com>
2021-08-29 12:35:01 +0300

    day#6 29.08.2021

---
+++ 8kyu/54 - Reversed Sequence/app.js
@@ -0,0 +1,11 @@
+/* Build a function that returns an array of integers from n to 1 where n>0.
+
+Example : n=5 --> [5,4,3,2,1] */
+
+const reverseSeq = (n) => {
+  let myArr = [];
+  for (let i = n; i > 0; i--) {
+    myArr.push(i);
+  }
+  return myArr;
+};
