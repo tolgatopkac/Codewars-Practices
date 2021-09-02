f3c12ff7cf674a3c8e279bbdc0839a3d2ed49dd2
Tolga Topkaç <tolgatopkac09@gmail.com>
2021-09-02 10:42:57 +0300

    day#9 01.09.2021

---
+++ 8kyu/78 - Simple Multiplication/app.js
@@ -0,0 +1,14 @@
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+function simpleMultiplication(number) {
+  if (number % 2 === 0) {
+    return number * 8;
+  } else if (number % 2 === 1) {
+    return number * 9;
+  }
+}
+
+// alternative
+function simpleMultiplication(number) {
+  return number % 2 === 0 ? number * 8 : number * 9;
+}
---
+++ 8kyu/86 - Convert a string to an array/app.js
@@ -0,0 +1,5 @@
+// Write a function to split a string and convert it into an array of words. For example:
+
+function stringToArray(string) {
+  return string.split(" ");
+}
