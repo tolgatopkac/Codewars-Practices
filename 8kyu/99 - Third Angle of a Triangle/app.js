4239bfb99f3cb2deedbb6565557d5438a6567d1c
Tolga Topkaç <tolgatopkac09@gmail.com>
2021-09-03 22:02:39 +0300

    day#10 02.09.2021

---
+++ 8kyu/99 - Third Angle of a Triangle/app.js
@@ -0,0 +1,15 @@
+/* You are given two interior angles (in degrees) of a triangle.
+
+Write a function to return the 3rd.
+
+Note: only positive integers will be tested. */
+
+function otherAngle(a, b) {
+  return 180 - (a + b);
+}
+
+otherAngle = (a, b) => 180 - (a + b);
+
+const otherAngle = function (a, b) {
+  return 180 - (a + b);
+};
